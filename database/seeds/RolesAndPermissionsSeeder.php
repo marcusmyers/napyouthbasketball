b91327e4f439ec0eb68afd97c76b7ee0b3873941
Mark Myers <mmyers@squaremouth.com>
2018-09-19 22:21:03 -0400

    Add ability to manage teams
    This commit adds the ability to manage teams for the website. This
    commit also adds the tests for the added management of teams as well.
    
    [699490015461389](https://app.asana.com/0/0/699490015461389/f)

---
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+        // create roles and assign existing permissions
+        Role::create(['name' => 'super_administrator']);
+    }
+}
