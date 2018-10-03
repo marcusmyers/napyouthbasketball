6a8440c9ce093529f9c5d479ae2c707bdb08b333
Mark Myers <mmyers@squaremouth.com>
2018-10-03 11:37:54 -0400

    Create master layout and add partials for footer & navbar
    In order to use a master layout for all views excluding the home page
    this commit creates a master layout. This commit also creates partials
    for the navbar and the footer so that they can be updated in one spot
    instead of multiple.
    
    [849128918359100](https://app.asana.com/0/0/849128918359100/f)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,36 +6,11 @@
 
         <title>Napoleon Youth Basketball</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
 				<div class="main">
-					<nav class="flex items-center justify-between bg-navy p-6">
-					  @include('partials.logo')
-					  <div class="block md:hidden">
-					    <button class="flex items-center px-3 py-2 border rounded text-navy-lighter border-navy-light hover:text-white hover:border-white">
-					      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-					    </button>
-					  </div>
-					  <ul class="list-reset hidden sm:flex  sm:li-pl-4 li-pb-4 md:li-pb-0 pin-r">
-					    <!-- <div class="text-sm lg:flex-grow"> -->
-					      <li><a href="#responsive-header" class="font-sans block mt-4 lg:inline-block lg:mt-0 text-navy-lighter no-underline hover:text-white mr-4">
-					        Teams
-					      </a></li>
-
-					      <li><a href="#responsive-header" class="font-sans block mt-4 lg:inline-block lg:mt-0 text-navy-lighter no-underline hover:text-white mr-4">
-					        Photos/Videos
-					      </a></li>
-					      <li><a href="#responsive-header" class="font-sans block mt-4 lg:inline-block lg:mt-0 text-navy-lighter no-underline hover:text-white">
-					        Forms
-					      </a>
-					    	</li>
-					    </ul>
-					    <!-- </div> -->
-					  </div>
-					</nav>
+					@include('partials.navbar')
 	    		<div class="bg-cover-image lg:h-160">
 	    			<img src='https://res.cloudinary.com/dblyrab9o/image/upload/v1538146117/napyouthbball/CoverPhoto/jesse-orrico-586601-unsplash.jpg'>
 	    		</div>
@@ -62,50 +37,6 @@
           	</div>
           </div>
 
-          <footer class="flex flex-row w-full h-32 shadow bg-navy-light">
-          	<div class="container mx-auto items-center flex">
-          		<div class="footer-menu w-1/2 text-left">
-          			<ul class="list-reset sm:flex text-lg flex-col text-left">
-          				<li><a href="/teams" class="text-navy-darker no-underline hover:text-white">Teams</a></li>
-          				<li><a href="/teams" class="text-navy-darker no-underline hover:text-white">Photos & Videos</a></li>
-          				<li><a href="/teams" class="text-navy-darker no-underline hover:text-white">Forms</a></li>
-          			</ul>
-          		</div>
-          		<div class="footer-menu w-1/2 text-right">
-	          		<div class="social-icons pin-r">
-	          			<a target="_blank" href="https://www.twitter.com/napyth">
-								      <svg xmlns="http://www.w3.org/2000/svg"
-												aria-label="Twitter" role="img"
-												viewBox="0 0 512 512"><rect
-												width="512" height="512"
-												rx="15%"
-												fill="#1da1f3"/>
-												<path fill="#fff" d="M437 152a72 72 0 0 1-40 12 72 72 0 0 0 32-40 72 72 0 0 1-45 17 72 72 0 0 0-122 65 200 200 0 0 1-145-74 72 72 0 0 0 22 94 72 72 0 0 1-32-7 72 72 0 0 0 56 69 72 72 0 0 1-32 1 72 72 0 0 0 67 50 200 200 0 0 1-105 29 200 200 0 0 0 309-179 200 200 0 0 0 35-37"/></svg>
-								  </a>
-
-		          		<a target="_blank" href="https://www.facebook.com/groups/280957275856227/">
-								      <svg xmlns="http://www.w3.org/2000/svg"
-								            aria-label="Facebook" role="img"
-								            viewBox="0 0 512 512">
-								           <rect
-								                   width="512" height="512"
-								                   rx="15%"
-								                   fill="#3b5998"/>
-								           <path fill="#fff"
-								                 d="M330 512V322h64l9-74h-73v-47c0-22 6-36 37-36h39V99c-7-1-30-3-57-3-57 0-95 34-95 98v54h-64v74h64v190z"/>
-								      </svg>
-								  </a>
-								</div>
-          		</div>
-          	</div>
-          </footer>
-	        <div class="bg-navy-darkest text-center text-white no-underline h-8 items-center">
-	        	<div class="items-center container mx-auto">
-	        		<p class="pt-2">
-		        		Website by <a href="https://www.bandmtechnologies.com" class="text-navy-lighter no-underline hover:text-white">B & M Technologies</a>
-		        	</p>
-	        	</div>
-	        </div>
-	      </div>
+          @include('partials.footer')
     </body>
 </html>
