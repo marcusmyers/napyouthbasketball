f985dcc1a090aad9e95d148b2c6634ad316070b7
Mark Myers <mmyers@squaremouth.com>
2018-11-15 14:33:21 -0500

    Add Videos to be used with coaches to view
    In order to allow coaches to watch just instructional videos, this
    commit adds the Video model and the correct policy for the coaches role.
    
    [902358026441132](https://app.asana.com/0/0/902358026441132/f)

---
+++ app/Nova/Video.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Inspheric\Fields\Url;
+use Laravel\Nova\Fields\File;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Video extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Video';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->hideFromIndex(),
+            Text::make('Title')->sortable(),
+            Url::make('Url')
+                ->rules('url')
+                ->clickableOnIndex()
+                ->clickable(),
+            Select::make('Type')->options([
+                'Instructional' => 'Instructional',
+                'Web' => 'Web',
+            ]),
+            // File::make('File')->store(new StoreForms),
+            // Text::make('File Name')->exceptOnForms(),
+
+            // Text::make('File Size')
+            //     ->exceptOnForms()
+            //     ->displayUsing(function ($value) {
+            //         return number_format($value / 1024, 2).'kb';
+            //     }),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
