5f1708d9bf3b3e54b1418e67ce1c6cf7754b6ba5
Mark Myers <mmyers@squaremouth.com>
2018-12-07 14:35:59 -0500

    Order events on schedules page and add practices to team pages
    In order to make it easier to read this commit orders the games and
    practices on the schedules page.  This commit also adds the practice
    schedule to the teams pages.

--- app/Http/Controllers/SchedulesController.php
+++ app/Http/Controllers/SchedulesController.php
@@ -10,8 +10,8 @@ class SchedulesController extends Controller
 {
     public function index()
     {
-    	$practices = Practice::notOld()->whereNotNull('team_id')->get();
-    	$games = Game::all();
+    	$practices = Practice::notOld()->whereNotNull('team_id')->orderBy('practice_time', 'asc')->get();
+    	$games = Game::all()->sortBy('game_time');
 
     	return view('schedules.index', ['practices'=>$practices,'games'=>$games]);
     }
